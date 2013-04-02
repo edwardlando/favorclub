a4155e8b11691a4ce49d852a050d6f993b1340f8
edwardlando <edwardlando8@gmail.com>
2013-04-02 18:47:22 -0400

    added header, some of it has to be fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Favorclub::Application.routes.draw do
   resources :posts
 
 
+
   devise_for :users
+
+  get "users/show"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
